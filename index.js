74a22a10cbbe24f25a01d5ef41924d93c75223df
waqqasiq <waqqas.ess@gmail.com>
2022-10-17 13:44:48 +0600

    added comment req.body object of unique user

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.post('/subscribe', (req, res) => {
     console.log('/subscribe START');
     const subscription = req.body;
     console.log('req.body ', subscription);
+
     // console.log('req.query ', req.query);
 
     res.status(201).json({});
@@ -35,7 +36,17 @@ app.post('/subscribe', (req, res) => {
 
 })
 
-
-const port = 3022;
+const port = 3024;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+/* store /subscribe req.body object in db for sending future notification to all users
+    req.body  {
+    endpoint: 'https://fcm.googleapis.com/fcm/send/e6v-8iFiuNY:APA91bGYnmJSg8tM0QJYUiTJKhM6N7f6NsbjPYD9eLV0AfOfa7R1cSp_VvzkeuLcU5_YGCuD3xX2vog6xAnb-zif9fQpD_9zQhG5UuyLkY1O3k1FhUooHQRp42Pt_HyH06ltoUBi-duG',
+    expirationTime: null,
+    keys: {
+        p256dh: 'BMwtmiN_h2hWN23SpJoJGqdu35NWYLawKi2N1BL0y5jZkT2yJzS7lIRspi7q5cCCM-kwBjmTJ7Fa3g9-7lxz2CQ',
+        auth: '3GWr1BtVA3E4FTBSboYhYw'
+    }
+    }
+*/
