4c0182ec69ba0df2d00acf086cd6ff429001c74c
waqqasiq <waqqas.ess@gmail.com>
2022-10-17 13:45:41 +0600

    small formatting change

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/subscribe', (req, res) => {
 
     res.status(201).json({});
 
-    const payload = JSON.stringify({ title: "Push Notification"});
+    const payload = JSON.stringify({ title: "Push Notification" });
 
     // pass obj in sendNotification
     webpush
@@ -42,11 +42,11 @@ app.listen(port, () => console.log(`Server started on port ${port}`));
 
 /* store /subscribe req.body object in db for sending future notification to all users
     req.body  {
-    endpoint: 'https://fcm.googleapis.com/fcm/send/e6v-8iFiuNY:APA91bGYnmJSg8tM0QJYUiTJKhM6N7f6NsbjPYD9eLV0AfOfa7R1cSp_VvzkeuLcU5_YGCuD3xX2vog6xAnb-zif9fQpD_9zQhG5UuyLkY1O3k1FhUooHQRp42Pt_HyH06ltoUBi-duG',
-    expirationTime: null,
-    keys: {
-        p256dh: 'BMwtmiN_h2hWN23SpJoJGqdu35NWYLawKi2N1BL0y5jZkT2yJzS7lIRspi7q5cCCM-kwBjmTJ7Fa3g9-7lxz2CQ',
-        auth: '3GWr1BtVA3E4FTBSboYhYw'
-    }
+        endpoint: 'https://fcm.googleapis.com/fcm/send/e6v-8iFiuNY:APA91bGYnmJSg8tM0QJYUiTJKhM6N7f6NsbjPYD9eLV0AfOfa7R1cSp_VvzkeuLcU5_YGCuD3xX2vog6xAnb-zif9fQpD_9zQhG5UuyLkY1O3k1FhUooHQRp42Pt_HyH06ltoUBi-duG',
+        expirationTime: null,
+        keys: {
+            p256dh: 'BMwtmiN_h2hWN23SpJoJGqdu35NWYLawKi2N1BL0y5jZkT2yJzS7lIRspi7q5cCCM-kwBjmTJ7Fa3g9-7lxz2CQ',
+            auth: '3GWr1BtVA3E4FTBSboYhYw'
+        }
     }
 */
